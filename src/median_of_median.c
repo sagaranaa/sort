e3609dc988fa63c2445ce4301631102c6fb1df74
sagaranatsuki <sosplaystation@gmail.com>
2019-06-26 20:20:48 +0900

    test 1

--- src/median_of_median.c
+++ src/median_of_median.c
@@ -13,9 +13,6 @@ void swap(int *p, int *q){
   *q = tmp;
 }
 
-int median_of_median(int A[], int k){
-  
-}
 
 int quick_select(int A[], int n, int k){
   int i, j, pivot;
@@ -36,6 +33,46 @@ int quick_select(int A[], int n, int k){
   else return quick_select(A+1, j-1, k);
 }
 
+int median_of_median(int A[], int n, int k){
+  if(n<=5){
+    return quick_select(A, n, k);
+  }else{
+    int B[(n+4)/5];
+    int i,j,l,pivot;
+    int len_A = n;
+    int len_B = 0;
+
+    while(len_A>=5){
+      B[len_B] = median_of_median(A+5*len_B, 5, 2);
+      len_A-=5;
+      len_B++;
+    }
+    if(len_A!=0)B[len_B] = median_of_median(A+5*len_B,len_A,len_A/2);
+
+    pivot = median_of_median(B,len_B+1,(len_B+1)/2);
+    for(i = j = 1; i < n; i++){
+      if(A[i] < pivot){
+        swap(A+i, A+j);
+        j++;
+      }
+    }
+
+    for(i = l = j; i < n; i++){
+      if(A[i] == pivot){
+        swap(A+i, A+l);
+        l++;
+      }
+    }
+
+    if(j <= k+1){
+      if(l < k+1) return quick_select(A+l, n-l, k-l);
+      else return pivot;
+    }else return quick_select(A+1, j-1, k);
+
+  }
+
+}
+
 
 int main(){
   int i;
@@ -44,8 +81,9 @@ int main(){
   for(i=2;i<N;i++){
     A[i] = (long long int) A[i-1] * A[1] % N;
   }
+
   for(i=0;i<N;i++){
-    if(quick_select(A, N, i) != i) printf("ERROR %d %d\n", i, quick_select(A, N, i));
+    if(median_of_median(A, N, i) != i) printf("ERROR %d\n", i);
 //    printf("%d th element is %d\n", i, quick_select(A, N, i));
   }
 }
